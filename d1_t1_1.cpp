dfb1c41e25575510f9970967ca04e8d50a221b4d
Tokamak Sullivan <tokamakyuri@gmail.com>
2020-10-20 17:45:42 +0800

    update

---
+++ d1_t1_1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+struct BigInteger{
+	static const int BASE = 100000000;
+	static const int WIDTH = 8;
+	vector<int> s;
+	
+	BigInteger(long long num = 0) { *this = num;}
+	BigInteger operator = (long long num) {
+		s.clear();
+		do {
+			s.push_back(num % BASE);
+			num /= BASE;
+		} while (num > 0);
+		return *this;
+	}
+	BigInteger operator = (const string& str) {
+		s.clear();
+		int x, len = (str.length() - 1) / WIDTH + 1;
+		for (int i = 0; i < len; i++) {
+			int end = str.length() - i * WIDTH;
+			int start = max(0, end - WIDTH);
+			sscanf(str.substr(start, end - start).c_str(), "%d", &x);
+			s.push_back(x);
+		}
+		return *this;
+	}
+	BigInteger operator + (const BigInteger& b) const {
+		BigInteger c;
+		c.s.clear();
+		for (int i = 0, g = 0; ; i++) {
+			if (g == 0 && i >= s.size() && i >= b.s.size()) break;
+			int x = g;
+			if (i < s.size()) x += s[i];
+			if (i < b.s.size()) x += b.s[i];
+			c.s.push_back(x % BASE);
+			g = x / BASE;
+		}
+		return c;
+	}
+};
+
+const int MAXN = 100000 + 10;
+int n;
+BigInteger fib[MAXN];
+
+int main(){
+	cin >> n;
+	fib[1] = fib[2] = 1;
+	for(int i = 3; i <= n; i++) fib[i] = fib[i - 1] + fib[i - 2];
+	cout << fib[n];
+}

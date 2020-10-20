a274628d76ccec0cac02ff16070f371559a7cbaa
Tokamak Sullivan <tokamakyuri@gmail.com>
2020-10-20 16:40:45 +0800

    Create d1_t1.cpp

---
+++ d1_t1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+
+using namespace std;
+
+int n;
+unsigned long long fib[100];
+
+int main(){
+	cin >> n;
+	fib[1] = fib[2] = 1;
+	for(int i = 3; i < 100; i++) fib[i] = fib[i - 1] + fib[i - 2];
+	cout << fib[n];
+	return 0;
+}
